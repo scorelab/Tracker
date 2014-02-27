d655e4e23ba24f0f015a2012eaa115b1f3ef7f91
agentmilindu <agentmilindu@gmail.com>
2014-02-27 18:31:59 +0530

    Android module added

---
+++ MobilityTrackAndroid/gen/lk/ucsc/apps/mobilitytrackandroid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package lk.ucsc.apps.mobilitytrackandroid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ helloflashlight/gen/com/simpligility/android/helloflashlight/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.simpligility.android.helloflashlight;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
