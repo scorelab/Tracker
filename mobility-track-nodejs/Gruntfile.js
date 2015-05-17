89e6c28d8512c7f71fdfa7e16fe6b84869405637
Malith Senaweera <malithsenaweera@gmail.com>
2015-05-17 18:44:32 +0530

    Add grunt, jshint, nodemon, concurrent, contrib-watch

---
+++ mobility-track-nodejs/Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: {
+        src: ['*.js', 'public/js/*.js']
+      }
+    },
+    watch: {
+      js: {
+        files: ['*.js', 'public/js/*.js'],
+        tasks: ['jshint']
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'server.js',
+        options: {
+          args: [],
+          ignoredFiles: ['node_modules/**'],
+          ignore: ['public/**'],
+          watchedExtensions: ['js'],
+          debug: true,
+          delayTime: 1,
+          env: {
+            PORT: 3000
+          },
+          cwd: __dirname
+        }
+      }
+    },
+    concurrent: {
+      tasks: ['nodemon', 'watch'],
+      options: {
+        logConcurrentOutput: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('default', ['jshint', 'concurrent']);
+};
