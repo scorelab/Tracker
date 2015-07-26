b48dfe504a5b27d84d770e784ac553bab54901f9
Sisinda Dinusha <sisindaa@gmail.com>
2015-07-26 11:58:00 +0530

    Get device MAC address. Used rmnet0 interface.

--- mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/Constants.java
+++ mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
     public static final String SERVER_URL = "http://192.168.1.4:3000/api/tracker/location/data";
     public static final int UPDATE_FREQUENCY = 2000;
+    public static final String RMNET0_ADDRESS_FILE_PATH = "/sys/class/net/rmnet0/address";
 }
 
--- mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/MainActivity.java
+++ mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/MainActivity.java
@@ -7,13 +7,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    public static String deviceMAC = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            getDeviceMAC();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -48,6 +60,14 @@ public class MainActivity extends ActionBarActivity {
     public void stop(View v) {
         Intent intent = new Intent(this, LocationUpdates.class);
         stopService(intent);
+    }
+
+    private void getDeviceMAC() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(Constants.RMNET0_ADDRESS_FILE_PATH));
+        for (String block : reader.readLine().split(":")) {
+            deviceMAC += block.toUpperCase();
+        }
+        reader.close();
     }
 
     /*public boolean isServiceRunning(Class<?> serviceClass) {
