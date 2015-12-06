f8e962c42542293f77fce0dd5c40e14d38e6e962
tharindupr <tharindu.prf@gmail.com>
2015-12-06 12:57:06 +0530

    Angular migration initialized

---
+++ mobility-track-web/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webappApp
+ * @description
+ * # webappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webappApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
