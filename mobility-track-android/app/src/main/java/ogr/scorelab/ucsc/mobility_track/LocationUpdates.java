fe91abf388c7f4167f71ddb0208e6c6a3993f7a4
Sisinda Dinusha <dinush@debian>
2016-01-24 10:08:04 +0530

    Split a function

--- mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/LocationUpdates.java
+++ mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/LocationUpdates.java
@@ -16,6 +16,7 @@ import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.DataOutputStream;
@@ -117,6 +118,54 @@ Log.i("TRACKER", "Service on start.");
         }
     }
 
+    private JSONObject getJsonObject(Location2 location2) throws JSONException {
+        JSONObject holder = new JSONObject();
+
+        String key = "id";
+        String data = deviceId;
+        holder.put(key, data);
+
+        key = "status";
+        holder.put(key, 1);
+
+        holder.put("timestamp", location2.timestamp);
+
+        JSONArray dataArray = new JSONArray();
+        JSONObject dataObj = new JSONObject();
+        dataObj.put("latitude", location2.latitude);
+        dataObj.put("longitude", location2.longitude);
+        dataObj.put("direction", location2.direction);
+        dataObj.put("speed", location2.speed);
+        dataObj.put("timestamp", location2.timestamp);
+        dataArray.put(dataObj);
+        holder.put("data", dataArray);
+
+        return holder;
+    }
+
+    private boolean sendJsonToServer (JSONObject dataHolder)
+    {
+        boolean ret = true;     // Return value
+
+        try {
+            DataOutputStream out = new DataOutputStream(httpConnection.getOutputStream());
+            out.write(dataHolder.toString().getBytes("UTF-8"));
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            // Data send failed
+            ret = false;
+        } finally {
+            try {
+                Log.d("TRACKER",httpConnection.getResponseMessage());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            httpConnection.disconnect();
+        }
+        return ret;
+    }
+
     public boolean packAndPost(Location2 location2)
             throws Exception {
         boolean ret = true; // Return value
@@ -210,11 +259,13 @@ Log.i("TRACKER", "Service on start.");
                     continue;
                 }
                 try {
-                    if (packAndPost(l2)) {  // if data posted to the server successfully
-                        dbAccess.delete(l2.timestamp);
-                    } else {
+                    JSONObject jsonDataPacket = getJsonObject(l2);
+
+                    if (sendJsonToServer(jsonDataPacket))
+                        dbAccess.delete(l2.timestamp);      // Remove transferred item from database
+                    else
                         Thread.sleep(1000);
-                    }
+
                 } catch (Exception e) {
                     e.printStackTrace();
                     break;
