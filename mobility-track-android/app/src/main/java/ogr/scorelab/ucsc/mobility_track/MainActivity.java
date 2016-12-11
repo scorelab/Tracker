159badd4f4a1e3fc778dcb2b59c52f78c08e9ac0
unknown <tsmpmail@gmail.com>
2016-12-11 08:22:18 +0530

    Added Toast Notification when theres no network access

--- mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/MainActivity.java
+++ mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -54,6 +56,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map));
         map.getMapAsync(this);
+
+        if(isNetworkAvailable()==false){
+            Toast.makeText(MainActivity.this, "You need to have an active internet connection!", Toast.LENGTH_LONG).show();
+        }
 
         getDeviceId();
     }
@@ -208,5 +214,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             return;
         }
         googleMap.setMyLocationEnabled(true);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
