a8f5686e5475db88b59cc055d7cdbf54230260be
Jeff Sieu <jeffsy00@gmail.com>
2016-12-11 12:09:50 +0800

    Updated app UI to fit material design guidelines

--- mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/SettingsActivity.java
+++ mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/SettingsActivity.java
@@ -1,10 +1,12 @@
 package ogr.scorelab.ucsc.mobility_track;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,9 +20,21 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView mapsAttrTxt = ((TextView) findViewById(R.id.mapAttributionTxt));
         mapsAttrTxt.setText(GoogleApiAvailability.getInstance().getOpenSourceSoftwareLicenseInfo(this));
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
