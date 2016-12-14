7e0d6b545d274f59ea592a8a073db15a7578599e
reubenvarghese1 <reubenvarghese1@gmail.com>
2016-12-14 20:53:18 +0530

    add latest files

---
+++ mobility-track-android/app/src/main/java/ogr/scorelab/ucsc/mobility_track/Splash.java
@@ -0,0 +1,45 @@
+package ogr.scorelab.ucsc.mobility_track;
+
+/**
+ * Created by reubenvarghese1 on 14/12/16.
+ */
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.Handler;
+
+public class Splash extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
+
+
+
